43fe8920918906d4892a777bc6b491a3c7e43a0e
mohammadJahid8 <mohammadjahid0007@gmail.com>
2023-07-05 12:27:30 +0600

    [Fixed] initial state id issue

--- src/Context/TodoContext.js
+++ src/Context/TodoContext.js
@@ -4,9 +4,10 @@ export const TodoContext = createContext();
 
 const initialState = [
   { id: Date.now(), name: "I am a sample completed task", completed: true },
-  { id: Date.now(), name: "I am not completed", completed: false },
+  { id: Date.now() + 1, name: "I am a sample pending task", completed: false },
 ];
 
+console.log(initialState);
 export default function TodoProvider({ children }) {
   const [tasks, setTasks] = useState(initialState);
   const [filter, setFilter] = useState("all");
